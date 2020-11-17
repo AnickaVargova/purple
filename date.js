6cdc3778fa44da4a21e77d5a76adea9c516e6c0c
AnickaVargova <64069511+AnickaVargova@users.noreply.github.com>
2020-11-17 20:46:18 +0100

    first completed version

---
+++ date.js
@@ -0,0 +1,58 @@
+document.write(` <div id="date">
+<img id="calendar" src="assets/calendar(1).png" />
+<p>
+  <strong style="color: rgb(97, 96, 96)"
+    >15 of September
+    <span style="color: grey; margin-right: 5px">4pm CET</span>
+    <span id="info"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="20"
+        height="20"
+        viewBox="0 0 172 172"
+        style="fill: #000000"
+      >
+        <g
+          fill="none"
+          fill-rule="nonzero"
+          stroke="none"
+          stroke-width="1"
+          stroke-linecap="butt"
+          stroke-linejoin="miter"
+          stroke-miterlimit="10"
+          stroke-dasharray=""
+          stroke-dashoffset="0"
+          font-family="none"
+          font-weight="none"
+          font-size="none"
+          text-anchor="none"
+          style="mix-blend-mode: normal"
+        >
+          <path d="M0,172v-172h172v172z" fill="none"></path>
+          <g id="Layer_1">
+            <circle
+              cx="64"
+              cy="64"
+              transform="scale(1.34375,1.34375)"
+              r="55"
+              fill="#ffffff"
+            ></circle>
+            <path
+              d="M86,163.9375c-43,0 -77.9375,-34.9375 -77.9375,-77.9375c0,-43 34.9375,-77.9375 77.9375,-77.9375c43,0 77.9375,34.9375 77.9375,77.9375c0,43 -34.9375,77.9375 -77.9375,77.9375zM86,16.125c-38.56563,0 -69.875,31.30937 -69.875,69.875c0,38.56562 31.30937,69.875 69.875,69.875c38.56562,0 69.875,-31.30938 69.875,-69.875c0,-38.56563 -31.30938,-69.875 -69.875,-69.875z"
+              fill="#444b54"
+            ></path>
+            <path
+              d="M86,120.9375c-2.28437,0 -4.03125,-1.74688 -4.03125,-4.03125v-34.9375c0,-2.28437 1.74688,-4.03125 4.03125,-4.03125c2.28437,0 4.03125,1.74688 4.03125,4.03125v34.9375c0,2.28437 -1.74688,4.03125 -4.03125,4.03125z"
+              fill="#444b54"
+            ></path>
+            <path
+              d="M86,65.84375c-2.28437,0 -4.03125,-1.74688 -4.03125,-4.03125v-6.71875c0,-2.28438 1.74688,-4.03125 4.03125,-4.03125c2.28437,0 4.03125,1.74687 4.03125,4.03125v6.71875c0,2.28437 -1.74688,4.03125 -4.03125,4.03125z"
+              fill="#444b54"
+            ></path>
+          </g>
+        </g></svg></span
+  ></strong>
+</p>
+</div>`)
